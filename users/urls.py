da59669778c289e382f762f02619324bb94fc3d3
ronak-kapadia <ronakkapadia227@gmail.com>
2024-07-05 17:55:09 +0530

    logs

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('store/',views.store,name='store'),
+    # path('store/',views.store,name='store'),
     path('signup/',views.signup,name='signup'),
     path('login/', views.login, name='login')
 
--- users/views.py
+++ users/views.py
@@ -1,11 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login 
+
 
 def store(request):
     return render(request,'store/main.html')
      
 def signup(request) :
-    return render(request,'users/signup.html')
+
+    if request.method == 'POST' :
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+        
+        if pass1 == pass2:
+            my_user = User.objects.create_user(username=name, email=email, password=pass1)
+            my_user.save()
+
+            messages.success(request, "Your account has been successfully created")
+            return redirect('login')
+        else:
+            messages.error(request, "Passwords do not match")
+    
+    return render(request, 'users/signup.html')
+
+
 
 def login(request) :
+    if request.method =='POST' :
+        name = request.POST('name')
+        pass1 = request.POST('pass1')
+
+        #inbuild 
+        user = authenticate(name=name,password=pass1)
+
+        if user is not None :
+            login(request, user )
+        
+        else :
+            messages.error(request,"Bad Credintial...!!!")
+            return redirect('store')
+    
+
+
     return render(request,"users/login.html")
+
+
+
 
